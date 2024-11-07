f64a35ddab092da2cb2955602d009505f6459bcb
Om <om@Oms-MacBook-Pro.local>
2024-11-07 23:24:29 +0530

    shorter description

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,7 +11,7 @@ export default function Projects(){
       <div className=" relative z-10 grid grid-rows-4 md:grid-cols-2 md:grid-rows-2 gap-y-10 items-center justify-items-center">
         <ProjectCard src="/projects/editor.png" github="#" liveLink="#" title="Collaborative Text Editor " description="Real-time collaborative text editor using React, Quill.js, and Socket.io, featuring Markdown support, rich text formatting, and MongoDB for persistent storage." tags={["ReactJS","Socket.io","MongoDB"]}/>
 
-        <ProjectCard src="/projects/billBuddies.jpeg"  github="https://github.com/Itaro18/bill-buddies" liveLink="https://bill-buddies.vercel.app" title=" Bill Buddies" description="built Bill Buddies, a Splitwise clone using Next.js, PostgreSQL, Prisma, and Tailwind, allowing users to effortlessly track group expenses and split bills. The app offers real-time syncing, a user-friendly interface," tags={["NextJS","Tailwind","Postgres"]}/>
+        <ProjectCard src="/projects/billBuddies.jpeg"  github="https://github.com/Itaro18/bill-buddies" liveLink="https://bill-buddies.vercel.app" title=" Bill Buddies" description="Built  a Splitwise clone allowing users to effortlessly track group expenses and split bills. The app offers real-time syncing and a user-friendly interface" tags={["NextJS","Tailwind","Postgres"]}/>
 
         <ProjectCard src="/projects/lexer.png" github="#" liveLink="#" title="Lexer and Tokenizer in C++" description="Developed a C++ lexer for custom language tokenization, optimizing with classes, enums, and STL containers, enhancing compiler efficiency and maintainability." tags={["C++"]}/>
 
